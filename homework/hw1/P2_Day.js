972ee3aedf0819f8129645818b203ba06d8e62d4
gtfarng <5735512002@psu.ac.th>
2019-01-26 18:14:54 +0700

    homework_lab1

---
+++ homework/hw1/P2_Day.js
@@ -0,0 +1,20 @@
+const readline = require('readline').createInterface
+({
+  input: process.stdin,
+  output: process.stdout
+})
+
+console.log("This program to convert days to year, month and week  ")
+readline.question("Enter total day: ", (day) => 
+{
+      year=day/365
+      month=day/30
+      week=day/7
+      console.log(+day +" days is " +year.toFixed(0) + " year" )
+      console.log(+day +" days is " +month.toFixed(0) + " month" )
+      console.log(+day +" days is " +week.toFixed(0) + " week" )
+      
+      readline.close()
+      
+})
+
---
+++ homework/hw1/P3_factorial.js
@@ -0,0 +1,19 @@
+const readline = require('readline').createInterface
+({
+  input: process.stdin,
+  output: process.stdout
+})
+
+readline.question("Enter number : ", (n) => 
+{
+	 var factorial=1
+      for(let i=1;i<1+parseInt(n);i++)
+      {
+      	  factorial *= i
+      } 	
+      console.log(+n  +"! (factorial) = " +factorial +"\n" ) 	     
+     
+      readline.close()
+      
+})
+
