337d41350442fb7d901a4de97bbe332694d416d9
gtfarng <5735512002@psu.ac.th>
2019-03-12 01:09:15 +0700

    update-lab6

---
+++ Lab6-Rest_API_&_React_Redux/lab/My-App/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>My-App</h1>         
+        </header>
+        <div>
+         
+
+        </div>
+      </div>
+    );
+  }
+}
+
+
