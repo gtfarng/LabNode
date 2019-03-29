10037e2bf0b98f3fb8d1ca37ba86f5d8f512b79e
gtfarng <5735512002@psu.ac.th>
2019-03-29 15:11:24 +0700

    update

--- Lab7-Security_and_OAuth/Oauth/react-psu/src/App.js
+++ Lab7-Security_and_OAuth/Oauth/react-psu/src/App.js
@@ -3,16 +3,41 @@ import './App.css';
 import axios from 'axios'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+
+  handleChangeUsername = (e) => this.setState({ username: e.target.value },
+    console.log("Username:",this.state.username)
+  )
+
+  handleChangePassword = (e) => this.setState({ password: e.target.value },
+  console.log("Password:",this.state.password)
+  )
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1>My-React_APP</h1>
+      <div className="App" align="center">
+        <header className="App-header"><br />
+          <h1>My-React_APP</h1><br />
         </header>
 
+        <br /> <h2>PSU Passport Authentication (SOAP)</h2><br />
+        <form action="/" method="post" >
+          USERNAME : &nbsp; <input type="text" placeholder="Username" name="username" onChange={this.handleChangeUsername} value={this.state.username} /> <br /><br />
+          PASSWORD : &nbsp; <input type="password" placeholder="Password" name="password" onChange={this.handleChangePassword} value={this.state.password} /> <br /><br />
+          <input type="submit" value="Submit" />
+        </form>
 
+
+        <br />
         <div>
-
+          <h2>Render PSU</h2>
 
 
         </div>
