945f950812da57af3ba22841620d1570c40f1b95
gtfarng <5735512002@psu.ac.th>
2019-04-09 14:30:07 +0000

    update

---
+++ Lab9-Web_deployment/lab9/run_load_test_host.sh
@@ -0,0 +1,2 @@
+docker run --net="host" -i loadimpact/k6 run --vus 100 --duration 20s  - < script_host.js
+
---
+++ Lab9-Web_deployment/lab9/script.js
@@ -0,0 +1,10 @@
+import {check,sleep} from 'k6'
+import http from 'k6/http'
+
+export default () => {
+   let res=http.get('http://gtfarnghost.ddns.net')
+// let res=http.get('https://httpbin.org') 
+  check(res, { "is status 200": (r) => r.status === 200 })
+   sleep(3)
+}
+
