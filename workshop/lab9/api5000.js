3625914e108a39cccc07af063b69d4d098de19e7
gtfarng <5735512002@psu.ac.th>
2019-04-19 12:24:58 +0700

    update

--- workshop/lab9/api5000.js
+++ workshop/lab9/api5000.js
@@ -1,4 +1,4 @@
 var express = require('express');
 var app = express();
-app.get('/api', (req, res) => res.send('Hello world on PORT 5000') );
+app.get('/apis', (req, res) => res.send('Hello world on PORT 5000') );
 app.listen(5000);
